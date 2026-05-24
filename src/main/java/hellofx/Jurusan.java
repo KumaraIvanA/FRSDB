c6ebb8a0e766500ed359bcdba49d8bcc90ac765a
hanselleslie <hanselleslie54@gmail.com>
2026-05-24 22:10:23 +0700

    Penambahan Kelas Jurusan

---
+++ src/main/java/hellofx/Jurusan.java
@@ -0,0 +1,30 @@
+package hellofx;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Jurusan {
+    private SimpleIntegerProperty idJurusan;
+    private SimpleStringProperty namaJurusan;
+
+    public Jurusan(int idJurusan, String namaJurusan) {
+        this.idJurusan = new SimpleIntegerProperty(idJurusan);
+        this.namaJurusan = new SimpleStringProperty(namaJurusan);
+    }
+
+    public int idJurusan() {
+        return this.idJurusan.get();
+    }
+
+    public String getnamaJurusan() {
+        return this.namaJurusan.get();
+    }
+
+    public SimpleIntegerProperty idJurusanProperty() {
+        return idJurusan;
+    }
+
+    public SimpleStringProperty namaJurusanProperty() {
+        return namaJurusan;
+    }
+}

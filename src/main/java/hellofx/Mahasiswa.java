e2f4a144a43e5d5421501b5d28267f47a18c352a
KuruKuru375 <kurukuru2165@gmail.com>
2026-05-25 21:41:35 +0700

    Penambahan kelas Mahasiswa

---
+++ src/main/java/hellofx/Mahasiswa.java
@@ -0,0 +1,40 @@
+package hellofx;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Mahasiswa {
+    private SimpleStringProperty npm;
+    private SimpleStringProperty nama;
+    private SimpleStringProperty email;
+    private SimpleIntegerProperty idJurusan;
+
+    public Mahasiswa(String npm, String nama, String email, int idJurusan) {
+        this.npm = new SimpleStringProperty(npm);
+        this.nama = new SimpleStringProperty(nama);
+        this.email = new SimpleStringProperty(email);
+        this.idJurusan = new SimpleIntegerProperty(idJurusan);
+    }
+
+    public String getNPM() {
+        return this.npm.get();
+    }
+
+    public String getNama() {
+        return this.nama.get();
+    }
+
+    public String getEmail() {
+        return this.email.get();
+    }
+
+    public int getIDJurusan() {
+        return this.idJurusan.get();
+    }
+
+    public SimpleStringProperty npmProperty(){ return npm;}
+    public SimpleStringProperty namaProperty(){ return nama;}
+    public SimpleStringProperty emailProperty(){ return email;}
+    public SimpleIntegerProperty idJurusanProperty(){ return idJurusan;}
+
+}

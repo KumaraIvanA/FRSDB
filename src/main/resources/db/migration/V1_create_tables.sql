3dc15d62fbc79a112999ed017b7169e3c24f06c2
Hansel <hanselleslie54@gmail.com>
2026-06-07 16:53:55 +0700

    new dummy data and remove log aktivitas

--- src/main/resources/db/migration/V1_create_tables.sql
+++ src/main/resources/db/migration/V1_create_tables.sql
@@ -1,6 +1,6 @@
+-- ---------- DROP (urutan child -> parent) ----------
 DROP TABLE IF EXISTS Teaches
 DROP TABLE IF EXISTS Enroll
-DROP TABLE IF EXISTS LogAktivitas
 DROP TABLE IF EXISTS MataKuliah
 DROP TABLE IF EXISTS FRS
 DROP TABLE IF EXISTS Dosen
@@ -9,12 +9,12 @@ DROP TABLE IF EXISTS Semester
 DROP TABLE IF EXISTS Jurusan
 
 
+-- ---------- CREATE TABLE ----------
 CREATE TABLE Jurusan
 (
-	idJurusan int IDENTITY(1, 1) PRIMARY KEY, 
+	idJurusan int IDENTITY(1, 1) PRIMARY KEY,
 	namaJurusan varchar(75) NOT NULL
 )
-
 
 CREATE TABLE Semester
 (
@@ -22,7 +22,6 @@ CREATE TABLE Semester
 	tahunAjaran char(9) NOT NULL,
 	jenis varchar(6) NOT NULL
 )
-
 
 CREATE TABLE Mahasiswa
 (
@@ -33,7 +32,6 @@ CREATE TABLE Mahasiswa
 	idJurusan int FOREIGN KEY REFERENCES Jurusan (idJurusan) NOT NULL
 )
 
-
 CREATE TABLE Dosen
 (
 	nip char(18) NOT NULL PRIMARY KEY,
@@ -43,21 +41,11 @@ CREATE TABLE Dosen
 	idJurusan int FOREIGN KEY REFERENCES Jurusan (idJurusan) NOT NULL
 )
 
-
-CREATE TABLE LogAktivitas(
-	idLog int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
-	npm char(10) FOREIGN KEY REFERENCES Mahasiswa(npm) NOT NULL,
-	tanggalWaktu datetime NOT NULL,
-	aktivitas varchar(255) NOT NULL,
-)
-
-
 CREATE TABLE FRS
 (
 	idFRS int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
-	idSemester int FOREIGN KEY REFERENCES Semester (idSemester) NOT NULL,
+	idSemester int FOREIGN KEY REFERENCES Semester (idSemester) NOT NULL
 )
-
 
 CREATE TABLE MataKuliah
 (
@@ -68,27 +56,25 @@ CREATE TABLE MataKuliah
 	idSemester int FOREIGN KEY REFERENCES Semester (idSemester) NOT NULL
 )
 
-
-CREATE TABLE Enroll 
+CREATE TABLE Enroll
 (
-    npm char(10) FOREIGN KEY REFERENCES Mahasiswa (npm) NOT NULL,
-    kodeMK int FOREIGN KEY REFERENCES MataKuliah (kodeMK) NOT NULL,
+	npm char(10) FOREIGN KEY REFERENCES Mahasiswa (npm) NOT NULL,
+	kodeMK int FOREIGN KEY REFERENCES MataKuliah (kodeMK) NOT NULL,
 	idSemester int FOREIGN KEY REFERENCES Semester (idSemester) NOT NULL,
-    idFRS int FOREIGN KEY REFERENCES FRS (idFRS) NOT NULL,
-    tanggalFRS datetime NOT NULL
+	idFRS int FOREIGN KEY REFERENCES FRS (idFRS) NOT NULL,
+	tanggalFRS datetime NOT NULL
 )
-
 
 CREATE TABLE Teaches
 (
 	idSemester int FOREIGN KEY REFERENCES Semester (idSemester) NOT NULL,
-    kodeMK int FOREIGN KEY REFERENCES MataKuliah (kodeMK) NOT NULL,
-    nip char(18) FOREIGN KEY REFERENCES Dosen (nip) NOT NULL,
-    kelas varchar(10) NOT NULL,
+	kodeMK int FOREIGN KEY REFERENCES MataKuliah (kodeMK) NOT NULL,
+	nip char(18) FOREIGN KEY REFERENCES Dosen (nip) NOT NULL,
+	kelas varchar(10) NOT NULL,
 	waktuMulai time,
-    durasi int NULL,
+	durasi int NULL,
 	hari varchar(10),
-    jenisPertemuan varchar(50) NULL,
-    metodePertemuan varchar(50) NULL,
-	PRIMARY KEY (idSemester, kodeMK, nip, hari)
+	jenisPertemuan varchar(50) NULL,
+	metodePertemuan varchar(50) NULL,
+	PRIMARY KEY (idSemester, kodeMK, nip, kelas, hari)
 )
